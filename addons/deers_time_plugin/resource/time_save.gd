f61ba356c0aff2934f380d2aadf9d62f7ca4dadc
LAPTOP-G1BCR7PC\Lenovo <17359672787@163.com>
2025-07-01 14:46:42 +0800

    提交项目计时器的1.0版本

---
+++ addons/deers_time_plugin/resource/time_save.gd
@@ -0,0 +1,7 @@
+extends Resource
+# TODO 时间资源 ===============>变 量<===============
+#region 变量
+@export var time : float = -1
+@export var use_time : float = -1
+@export var open_time : float = -1
+#endregion
